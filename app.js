803139dd00d62af357e207c3b4c1b02406f65620
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2017-07-19 10:28:00 +0000

    :zap: add base sparql request for translate keyword

--- app.js
+++ app.js
@@ -8,24 +8,34 @@ var client = new sparql.Client('http://dbpedia.org/sparql')
 app.get('/', function(req, res) {
   const query = url.parse(req.url, true).query
   const keyword = query.keyword
-  rus2eng(keyword)
-  res.send('Hello SPARQL!')
+  console.log(keyword)
+  rus2eng(keyword, function (result) {
+    res.send(result)
+  })
 })
 
 const port = process.env.PORT || 3000
 const host = process.env.IP || '0.0.0.0'
 
 app.listen(port, host, function () {
-  console.log('Example app listening on port ' + port)
+  console.log(`Example app listening on port: ${port} and host: ${host}`)
 })
 
-function rus2eng (keyword) {
-  client.query(`
-    SELECT *
+function rus2eng (keyword, callback) {
+  console.log('build query')
+  const query = `
+    SELECT DISTINCT ?englabel
     WHERE {
-      ?s ?p ?o
-    } LIMIT 10
-  `, function (_err, res) {
-    console.log(res.results.bindings[0])
+      ?entity rdfs:label ?label .
+      ?label bif:contains '"${keyword.toLowerCase()}"' .
+      ?entity rdfs:label ?englabel .
+      FILTER ( strlen(?label) = ${keyword.length} ) .
+      FILTER ( lang(?englabel) = 'en' )
+    } LIMIT 1
+  `
+  console.log(query)
+  client.query(query, function (err, res) {
+    console.log('exec query')
+    callback(res.results.bindings)
   })
 }
