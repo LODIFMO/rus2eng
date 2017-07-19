20f1247611fe81e1666883f6c3d80852c7321f9d
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2017-07-19 07:39:07 +0000

    :heavy_plus_sign: add sparql client package

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const url = require('url')
+const sparql = require('sparql')
+
+var client = new sparql.Client('http://dbpedia.org/sparql')
 
 app.get('/', function(req, res) {
   const query = url.parse(req.url, true).query
-  console.log(query.keyword)
+  const keyword = query.keyword
+  rus2eng(keyword)
   res.send('Hello SPARQL!')
 })
 
@@ -14,3 +18,14 @@ const host = process.env.IP || '0.0.0.0'
 app.listen(port, host, function () {
   console.log('Example app listening on port ' + port)
 })
+
+function rus2eng (keyword) {
+  client.query(`
+    SELECT *
+    WHERE {
+      ?s ?p ?o
+    } LIMIT 10
+  `, function (_err, res) {
+    console.log(res.results.bindings[0])
+  })
+}
